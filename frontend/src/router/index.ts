5a0ea4789f6a6ae6ac3bff4083cbb10371968de8
Александр Ефанов <verpl01@yandex.ru>
2021-06-17 03:06:01 +0500

    Add Frontend + Creating the App Layout

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,26 @@
+import Inventory from "@/views/Inventory.vue";
+import Vue from "vue";
+import VueRouter, {RouteConfig} from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'home',
+    component: Inventory
+  },
+  {
+    path: '/inventory',
+    name: 'inventory',
+    component: Inventory
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
